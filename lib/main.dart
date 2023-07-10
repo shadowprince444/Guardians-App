847e8eda3d03ae610f7a59d9f73a3785ce194ba3
Midhun-Narayanan-T <midhunnarayanan888@gmail.com>
2023-07-11 01:02:30 +0530

    Integrated firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_adoption_app/views/home/home_screen.dart';
 
-void main() {
-  runApp(PetAdoptionApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const PetAdoptionApp());
 }
 
 class PetAdoptionApp extends StatelessWidget {
